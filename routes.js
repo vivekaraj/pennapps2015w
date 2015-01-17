67c5846762a471559510b4ca7e71a489fb2a3176
hari102 <santhanarh@gmail.com>
2015-01-17 15:22:13 -0500

    second commit

--- routes.js
+++ routes.js
@@ -23,7 +23,7 @@ fee = [];
 
 var Postmates = require('postmates');
 
-var postmates = new Postmates('cus_KAefIoO_AD5TbV', '334e74ce-2d20-4055-9480-f39f9a385e12');
+var postmates = new Postmates('', '');
 
 var delivery = {
   pickup_address: pickup_address,
