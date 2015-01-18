ff00cfe80c31b1efca651bc320ff4bd129b67986
hari102 <santhanarh@gmail.com>
2015-01-17 19:47:22 -0500

    leaving pennapps

---
+++ postmate.js
@@ -0,0 +1,4 @@
+
+var postmates = new Postmates('cus_KAefIoO_AD5TbV', '334e74ce-2d20-4055-9480-f39f9a385e12');
+
+module.exports = postmates;
---
+++ yelp.js
@@ -0,0 +1,10 @@
+  var yelp = require("yelp").createClient({
+    consumer_key: "lwlQA9w2EapgsctWJUUn2g", 
+    consumer_secret: "KSJsvBmxNG9kkLKxBZlGqXQnHKU",
+    token: "yW9O-jRUu1aAb2hq2WbAgHUi0fdwjgKW",
+    token_secret: "ezU7CoWkhL-8pYf2ay6-UjfJhAw"
+
+  });
+
+
+module.exports = yelp;
